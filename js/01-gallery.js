0aa2cae92c2d9409201c9f61dc0752cd5db4bdf6
MaksGlushko <maks2007glyshko2@gmail.com>
2023-10-05 15:37:23 +0200

    homework 8

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,3 +3,9 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const gallery = new SimpleLightbox('.gallery a');
+});
