7eb7e5cce3e018fafe9372185caa3443a00521dd
MaksGlushko <maks2007glyshko2@gmail.com>
2023-10-06 13:12:50 +0200

    homework 8

---
+++ src/js/02-video.js
@@ -0,0 +1,19 @@
+import Vimeo from '@vimeo/player';
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+console.log(player);
+console.log(iframe);
+
+player.on('timeupdate', throttle(function(data) {
+    const currentTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime.toString());
+}, 1000));
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+console.log(savedTime);
+if(savedTime) {
+    player.setCurrentTime(parseFloat(savedTime));
+}
